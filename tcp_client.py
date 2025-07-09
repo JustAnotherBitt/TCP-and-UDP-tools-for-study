bc9296d070a13565ef408d191ff3b3b2fd5429e2
Abyss <leticiadepatta@gmail.com>
2025-07-09 00:39:17 -0300

    Adjusted for testing

--- tcp_client.py
+++ tcp_client.py
@@ -1,14 +1,14 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "0.0.0.0" # change it for some host such as www.google.com for further testing
+target_port = 9998 # hange it for the default port for web (80) for further testing
 
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET = IPv4 // SOCK_STREAM = indicates that this will be a TCP client
 # connect the client
 client.connect((target_host,target_port))
 # send some data
-client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(b"GET / HTTP/1.1\r\nHost: localhost\r\n\r\n")
 # receive some data
 
 response = client.recv(4096)
